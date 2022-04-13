f62c1b34ab37f2a8859d1c3b6f8a729dd178bd46
crugless54 <101901730+crugless54@users.noreply.github.com>
2022-04-13 16:11:37 -0400

    Add files via upload

---
+++ Demon.java
@@ -0,0 +1,16 @@
+
+public class Demon {
+    int health;
+    int attack;
+    public void demon(int health, int attack){
+        this.attack = attack;
+        this.health = health;
+    }
+    public int getHealth(){
+        return this.health;
+    }
+    public int getAttack(){
+        return this.attack;
+    }
+
+}
---
+++ DireWolf.java
@@ -0,0 +1,15 @@
+public class DireWolf {
+    int health;
+    int attack;
+    public void direwolf(int health, int attack){
+        this.attack = attack;
+        this.health = health;
+    }
+    public int getHealth(){
+        return this.health;
+    }
+    public int getAttack(){
+        return this.attack;
+    }
+
+}
