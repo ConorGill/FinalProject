1e6f7a2d8cdcafd3f11f55ccf391bb87a36a1c21
Nathan <nharris@mail.umw.edu>
2022-04-17 01:17:35 -0400

    Set up some more files, enums need to be added

---
+++ Character.java
@@ -0,0 +1,9 @@
+public class Character {
+    private int Health;
+    private int Attack;
+    private String name;
+   // private Weapon equipedWeapon; enums need to be added to the repository before these can work, change this when they are added.
+   // private Armor equipedArmor;
+    //private int Defense; I'm assuming this is an int, but on the design DIA file it is just defense, so I'm leaving this commented out for now.
+    //Constructors go here
+}
