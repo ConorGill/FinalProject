38698f1057164d0f6980385fce749859e4677b37
ConorGill <94633176+ConorGill@users.noreply.github.com>
2022-04-18 17:53:00 -0400

    Update Stairs.java

--- Stairs.java
+++ Stairs.java
@@ -1,3 +1,19 @@
-public class Stairs implements Tiles{
-
+import java.awt.*;
+import java.util.*;
+public class Stairs extends Tile{
+    private int Xcor;
+    private int Ycor;
+    public Stairs(int x,int y){
+        Xcor=x;
+        Ycor=y;
+    }
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        g.setColor(Color.BLACK);
+        g.fillRect(Xcor*80+100,Ycor*80+100,10,30);
+        g.fillRect(Xcor*80+110,Ycor*80+110,10,20);
+        g.fillRect(Xcor*80+120,Ycor*80+120,10,10);
+        revalidate();
+    }
 }
