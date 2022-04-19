98bd71bdf3cc856d45a68fbbc61fd2f90fd6617e
crugless54 <101901730+crugless54@users.noreply.github.com>
2022-04-18 20:51:12 -0400

    Add files via upload

---
+++ Monster.java
@@ -0,0 +1,14 @@
+public class Monster {
+    int health;
+    int attack;
+    String name;
+    public int getHealth(){
+        return this.health;
+    }
+    public int getAttack(){
+        return this.attack;
+    }
+    public String getName(){
+        return this.name;
+    }
+}
