3aca6ee1898a16c1e49df927f7c22709474965c4
Nathan <nharris@mail.umw.edu>
2022-04-18 21:59:07 -0400

    I believe this should be good, the setter methods I can see messing up however, also we need user input in main and the default values for the users hp there.

--- Character.java
+++ Character.java
@@ -1,9 +1,37 @@
+
 public class Character {
-    private int Health;
-    private int Attack;
+    private int health;
+    private int attack;
     private String name;
-   // private Weapon equipedWeapon; enums need to be added to the repository before these can work, change this when they are added.
-   // private Armor equipedArmor;
-    //private int Defense; I'm assuming this is an int, but on the design DIA file it is just defense, so I'm leaving this commented out for now.
-    //Constructors go here
+    private Item equippedWeapon;
+    private Item equippedArmor;
+    private int defense;
+
+    public Character(int hp, int atk, String nme, Item wep, Item arm, int def){ //I think this should be good after user input is added to main, just make the default
+        //health 300 in main, and defense 1 or something.
+        this.health = hp;
+        this.attack = atk;
+        this.name = nme;
+        this.equippedWeapon = wep;
+        this.equippedArmor = arm;
+        this.defense = def;
+    }
+    public int getHealth() {
+        return health;
+    }
+    public int getAttack() {
+        return attack;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setHealth(int health) {
+        this.health = health;
+    }
+    public void setAttack(int attack){
+        this.attack = equippedWeapon.getStrength();
+    }
+    public void setDefense(int defense){
+        this.defense = equippedArmor.getStrength();
+    }
 }
