4f667b8e88963088b42409700247c222e728ea9f
crugless54 <101901730+crugless54@users.noreply.github.com>
2022-04-20 11:41:07 -0400

    Added comments

--- MovementListener.java
+++ MovementListener.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
+//reprints the floor after the player has moved, updating postions
 public class MovementListener implements ActionListener {
     private int modifier;
     private JFrame frame;
